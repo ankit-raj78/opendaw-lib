23d701400f0e52bf3229d4f9560fd6d38e73a06a
André Michelle <andre.michelle@gmail.com>
2025-05-15 13:32:42 +0200

    use bash in lib adds set -euo pipefail

--- rebuild.sh
+++ rebuild.sh
@@ -1,18 +1,24 @@
-#!/bin/bash
-clear
-set -e
+#!/usr/bin/env bash
+set -euo pipefail
+export TERM=${TERM:-dumb}
+if [[ -t 1 ]]; then clear; fi
+
 sh ./clean.sh || exit 1
-dirs=(std runtime dom dsp box box-forge jsx fusion)   # topological sorted by dependency
+
+dirs=(std runtime dom dsp box box-forge jsx fusion)
 for dir in "${dirs[@]}"; do
   echo "install $dir"
   (cd "$dir" && npm install) || exit 1
 done
+
 for dir in "${dirs[@]}"; do
   echo "build $dir"
   (cd "$dir" && npm run build) || exit 1
 done
+
 for dir in "${dirs[@]}"; do
   echo "test $dir"
-  (cd "$dir" && npm run test) || exit 1
+  (cd "$dir" && npm run test)  || exit 1
 done
+
 echo "done"
