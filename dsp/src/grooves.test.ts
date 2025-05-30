a5ff146689ba075b30cee518edb98b8417943ba9
André Michelle <andre.michelle@gmail.com>
2025-05-30 12:17:12 +0200

    adds OffsetGroove to tests

--- dsp/src/grooves.test.ts
+++ dsp/src/grooves.test.ts
@@ -1,26 +1,35 @@
 import {describe, expect, it} from "vitest"
-import {GrooveChain, GroovePattern, GroovePatternFunction} from "./grooves"
+import {Groove, GrooveChain, GroovePattern, GroovePatternFunction} from "./grooves"
 import {PPQN, ppqn} from "./ppqn"
 import {meCurve} from "std"
 
-const createGroove = (duration: ppqn, amount: number) => new GroovePattern({
+const createMEGroove = (duration: ppqn, amount: number) => new GroovePattern({
     duration: (): ppqn => duration,
     fx: x => meCurve(x, amount),
     fy: y => meCurve(y, -amount)
 } satisfies GroovePatternFunction)
 
+const createOffsetGroove = (offset: ppqn): Groove => ({
+    warp: (position: ppqn): ppqn => position + offset,
+    unwarp: (position: ppqn): ppqn => position - offset
+})
+
 describe("Grooves", () => {
     it("should be revertible", () => {
-        const G = createGroove(PPQN.SemiQuaver * 2, 0.5)
+        const G = createMEGroove(PPQN.SemiQuaver * 2, 0.5)
         expect(G.unwarp(G.warp(PPQN.SemiQuaver))).toBeCloseTo(PPQN.SemiQuaver, 7)
         expect(G.unwarp(G.warp(PPQN.SemiQuaver * 1.1))).toBeCloseTo(PPQN.SemiQuaver * 1.1, 7)
+        expect(G.unwarp(G.warp(PPQN.SemiQuaver * 1.7))).toBeCloseTo(PPQN.SemiQuaver * 1.7, 7)
     })
     it("should be chainable", () => {
-        const A = createGroove(PPQN.SemiQuaver * 2, 0.50)
-        const B = createGroove(PPQN.SemiQuaver * 3, 0.75)
-        const C = createGroove(PPQN.SemiQuaver * 7, 0.66)
-        const G = new GrooveChain([A, B, C])
+        const A = createMEGroove(PPQN.SemiQuaver * 2, 0.50)
+        const B = createMEGroove(PPQN.SemiQuaver * 3, 0.75)
+        const C = createOffsetGroove(PPQN.SemiQuaver)
+        const D = createMEGroove(PPQN.SemiQuaver * 7, 0.66)
+        const G = new GrooveChain([A, B, C, D])
         expect(G.unwarp(G.warp(PPQN.SemiQuaver))).toBeCloseTo(PPQN.SemiQuaver, 7)
         expect(G.unwarp(G.warp(PPQN.SemiQuaver * 1.1))).toBeCloseTo(PPQN.SemiQuaver * 1.1, 7)
+        expect(G.unwarp(G.warp(PPQN.SemiQuaver * 1.7))).toBeCloseTo(PPQN.SemiQuaver * 1.7, 7)
+        expect(G.unwarp(G.warp(PPQN.SemiQuaver * 42.7))).toBeCloseTo(PPQN.SemiQuaver * 42.7, 7)
     })
 })
