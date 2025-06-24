185381a7fb6603a91806b97247eb075183b8eddc
André Michelle <andre.michelle@gmail.com>
2025-06-24 06:09:48 +0200

    adds more logging

--- std/src/strings.ts
+++ std/src/strings.ts
@@ -4,6 +4,9 @@ export namespace Strings {
     export const hyphenToCamelCase = (value: string) => value
         .replace(/-([a-z])/g, (g: string) => g[1].toUpperCase())
 
+    export const fallback = (value: Nullish<string>, fallback: string): string =>
+        isDefined(value) && value.length > 0 ? value : fallback
+
     export const endsWithDigit = (str: string): boolean => /\d$/.test(str)
 
     export const nonEmpty = (str: Nullish<string>, fallback: string): string =>
